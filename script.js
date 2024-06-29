b98fe6b43d1f70a0e0c0f7443bbc799861446086
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-29 20:20:57 +0400

    add pause fnc in Timer class and make new timer

--- script.js
+++ script.js
@@ -11,8 +11,14 @@ class Timer {
     this.pauseButton = pauseButton;
 
     this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
   }
   start() {
     console.log("start");
   }
+  pause() {
+    console.log("pause");
+  }
 }
+
+const timer = new Timer(durationInput, startButton, pauseButton);
