beb67648e3352ac14893e07848e45d6cd299e79f
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-30 01:06:42 +0400

    select circle and write perimeter formul

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const durationInput = document.getElementById("duration");
 const startButton = document.getElementById("start");
 const pauseButton = document.getElementById("pause");
+const circle = document.querySelector("circle");
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
