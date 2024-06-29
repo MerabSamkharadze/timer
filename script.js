ffa09f99da6a37735f55ddf7348ecf1e1c7fd57f
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-29 20:16:05 +0400

    add input and two buttons and select them from js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 "use strict";
 
+const durationInput = document.getElementById("duration");
+const startButton = document.getElementById("start");
+const pauseButton = document.getElementById("pause");
+
 class Timer {
   constructor(durationInput, startButton, pauseButton) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener("click", this.start);
+  }
+  start() {
+    console.log("start");
   }
 }
