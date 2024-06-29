31ebbe64f2912f31b0815e7da7fe7b5fdc1bbc00
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-29 23:20:05 +0400

    add timer.js file

--- script.js
+++ script.js
@@ -4,48 +4,14 @@ const durationInput = document.getElementById("duration");
 const startButton = document.getElementById("start");
 const pauseButton = document.getElementById("pause");
 
-class Timer {
-  constructor(durationInput, startButton, pauseButton, callBacks) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-    this.time = 30;
-    if (callBacks) {
-      this.onStart = callBacks.onStart;
-    }
-
-    this.startButton.addEventListener("click", this.start);
-    this.pauseButton.addEventListener("click", this.pause);
-  }
-  start = () => {
-    if (this.onStart) {
-      this.onStart();
-    }
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-  pause = () => {
-    clearInterval(this.interval);
-  };
-  tick = () => {
-    if (this.timeRemaining <= 0) {
-      this.pause();
-    } else {
-      this.timeRemaining = this.timeRemaining - 1;
-    }
-  };
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-}
-
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
     console.log("timer started");
   },
-  onTick() {},
-  onComplete() {},
+  onTick() {
+    console.log("timer jus ticked down");
+  },
+  onComplete() {
+    console.log("timer is completed");
+  },
 });
